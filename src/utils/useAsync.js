0c79832c0516b00a2ecc6e580db52028c8c91763
shehab910 <shehabihab@hotmail.com>
2022-04-13 03:49:48 +0200

    disabled warning (react-hooks/exhaustive-deps)

--- src/utils/useAsync.js
+++ src/utils/useAsync.js
@@ -19,6 +19,7 @@ export default function useAsync(callback, dependencies = []) {
         }
         setLoading(false);
       },
+      // eslint-disable-next-line react-hooks/exhaustive-deps
       dependencies,
     )
     useEffect(() => {
